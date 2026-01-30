d12efbcace0bc1fa20e2651fb0e814423d280fc2
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-30 14:29:46 +0200

    update main and app

--- carVIFI/src/App.jsx
+++ carVIFI/src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import YourCars from "./pages/YourCars";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { logoutUser } from "./services/auth";
+import { useAuth } from "./ctx/AuthContext";
 
 function App() {
+  const { user } = useAuth();
+
   return (
-    <div>
-      <h1>Car Reminder App 🚗</h1>
-    </div>
+    <>
+      {user && (
+        <div style={{ padding: 16 }}>
+          <button onClick={logoutUser}>Logout</button>
+        </div>
+      )}
+
+      <Routes>
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/cars"element={
+            <ProtectedRoute>
+              <YourCars />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to={user ? "/cars" : "/login"} replace />}/>
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
--- carVIFI/src/main.jsx
+++ carVIFI/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'
-import './styles/main.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./ctx/AuthContext";
+import "./styles/main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
