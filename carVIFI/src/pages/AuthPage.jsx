02711b4a78450716d7274e6ac5eeb22d54178a65
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-30 14:48:21 +0200

    setting up firestore db logic

--- carVIFI/src/pages/AuthPage.jsx
+++ carVIFI/src/pages/AuthPage.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { registerUser, loginUser } from "../services/auth";
+import { createUserProfile } from "../services/firestore";
 
 function AuthPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+  const cred = await registerUser(email, password);
+  await createUserProfile(cred.user);
+};
 
   return (
     <div style={{ padding: 24 }}>
@@ -25,9 +31,7 @@ function AuthPage() {
       <br /><br />
 
       <button onClick={() => loginUser(email, password)}>Login</button>
-      <button onClick={() => registerUser(email, password)}>
-        Register
-      </button>
+      <button onClick={handleRegister}>Register</button>
     </div>
   );
 }
---
+++ carVIFI/src/services/firestore.js
@@ -0,0 +1,35 @@
+import {collection, doc, setDoc, addDoc, getDocs, query, where, serverTimestamp} from "firebase/firestore";
+import { db } from "./firebase";
+
+/* USERS */
+
+export const createUserProfile = async (user) => {
+  if (!user) return;
+
+  const ref = doc(db, "users", user.uid);
+
+  await setDoc(ref, {
+    email: user.email,
+    createdAt: serverTimestamp(),
+  });
+};
+
+/* CARS */
+
+export const addCar = async (userId, carData) => {
+  return addDoc(collection(db, "cars"), {
+    ...carData,
+    userId,
+    createdAt: serverTimestamp(),
+  });
+};
+
+export const getUserCars = async (userId) => {
+  const q = query(collection(db, "cars"), where("userId", "==", userId));
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
