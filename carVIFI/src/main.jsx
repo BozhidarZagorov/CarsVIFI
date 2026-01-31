32feeee6e658d79dfd0b5a7e9aba0cf81f4328d2
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-31 17:07:50 +0200

    add car ctx to the app

--- carVIFI/src/main.jsx
+++ carVIFI/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./ctx/AuthContext";
+import { CarsProvider } from "./ctx/CarsContext";
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <CarsProvider>
         <App />
+        </CarsProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
