7a5098e84c478eb4237e5843e952eb2939dae80b
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-02-10 19:15:55 +0200

    expired logic
    added notification for expired items and a 3 day cooldown period

--- carVIFI/src/utils/reminderCheck.js
+++ carVIFI/src/utils/reminderCheck.js
@@ -3,12 +3,14 @@ import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../services/firebase";
 
 const DAYS_BEFORE = 30;
+const EXPIRED_COOLDOWN_DAYS = 3;
 
 export const checkAndSendReminders = async (user, cars) => {
   const now = new Date();
 
   for (const car of cars) {
     const expiringItems = [];
+    const expiredItems = [];
 
     for (const [key, value] of Object.entries(car)) {
       if (!value?.expiresAt) continue;
@@ -18,10 +20,17 @@ export const checkAndSendReminders = async (user, cars) => {
 
       const alreadyNotified =
         value.lastNotifiedAt &&
-        (now - value.lastNotifiedAt.toDate()) <
-          1000 * 60 * 60 * 24 * 7; // 7-day cooldown
+        now - value.lastNotifiedAt.toDate() <
+          1000 * 60 * 60 * 24 * 7;
 
-      if (diffDays <= DAYS_BEFORE && diffDays > 0 && !alreadyNotified) {
+      const expiredNotified =
+        value.expiredNotifiedAt &&
+        now - value.expiredNotifiedAt.toDate() <
+          1000 * 60 * 60 * 24 * EXPIRED_COOLDOWN_DAYS;
+
+      if (diffDays <= 0 && !expiredNotified) {
+        expiredItems.push(key);
+      } else if (diffDays <= DAYS_BEFORE && !alreadyNotified) {
         expiringItems.push(key);
       }
     }
@@ -31,14 +40,29 @@ export const checkAndSendReminders = async (user, cars) => {
         to: user.email,
         carName: `${car.brand} ${car.model}`,
         items: expiringItems,
+        type: "expiring",
       });
+    }
 
-      // update lastNotifiedAt
+    if (expiredItems.length > 0) {
+      await sendReminderEmail({
+        to: user.email,
+        carName: `${car.brand} ${car.model}`,
+        items: expiredItems,
+        type: "expired",
+      });
+    }
+
+    if (expiringItems.length > 0 || expiredItems.length > 0) {
       const ref = doc(db, "cars", car.id);
       const updates = {};
 
       expiringItems.forEach((key) => {
         updates[`${key}.lastNotifiedAt`] = new Date();
+      });
+
+      expiredItems.forEach((key) => {
+        updates[`${key}.expiredNotifiedAt`] = new Date();
       });
 
       await updateDoc(ref, updates);
